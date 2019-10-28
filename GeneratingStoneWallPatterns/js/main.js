e564d28f4d6dd564d02dd3e00ee695e50619505e
Pau <41298931+Fenikkel@users.noreply.github.com>
2019-10-28 10:07:16 +0100

    test

--- GeneratingStoneWallPatterns/js/main.js
+++ GeneratingStoneWallPatterns/js/main.js
@@ -6,6 +6,37 @@
   m_Context.moveTo(0,0);
   m_Context.lineTo(200,100);
   m_Context.stroke();
+  console.log("HOLA CARACOLA");
+
+  var posicion = {
+    x: 10,
+    y: 20
+  }
+
+  var m_Node = new WallNode(posicion, null, null, null, null);
+
+  console.log(m_Node.position);
+  console.log(m_Node.upper);
+  console.log(m_Node.lower);
+  console.log(m_Node.right);
+  console.log(m_Node.left);
+
+
+  var posicion2 = {
+    x: 30,
+    y: 40
+  }
+
+
+  var m_Node2 = new WallNode(posicion2, null, null, m_Node, null);
+
+  console.log(m_Node2.position);
+  console.log(m_Node2.upper);
+  console.log(m_Node2.lower);
+  console.log(m_Node2.right);
+  console.log(m_Node2.left);
+
+
 
 
 }());
--- GeneratingStoneWallPatterns/js/wallNode.js
+++ GeneratingStoneWallPatterns/js/wallNode.js
@@ -1,7 +1,6 @@
 class WallNode {
-  constructor(posX, posY, upper,lower, right, left) {
-    this.posX = posX;
-    this.posY = posY;
+  constructor(position, upper,lower, right, left) {
+    this.position = position;
     this.upper = upper;
     this.lower = lower;
     this.right = right;
